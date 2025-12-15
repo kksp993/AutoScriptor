e557a355cded691e305bcd51eda904da59f67ac0
Kksp993 <Kksp993@foxmail.com>
2025-12-16 03:31:24 +0800

    Refactor navigation and task scripts to enhance functionality and stability. Updated click functions to ensure stable interactions, added new environment mappings for 洪荒遗境, and improved task execution flow in daily tasks. Adjusted logging for better debugging and refined task order in daily routines.

--- ZmxyOL/task/daily_task/login/login_other_character.py
+++ ZmxyOL/task/daily_task/login/login_other_character.py
@@ -6,9 +6,12 @@ from ZmxyOL.nav.envs.login import login
 from logzero import logger
 
 @register_task
-def login_other_role(character_index=0, character_name="请输入文本"):
+def login_other_role(character_index=0, character_name="请输入文本", clear_all=False, lianbao=False):
     ensure_in("登录")
+    logger.info(f"登录其他角色: {character_index}, {character_name}")
     login(character_index=character_index, character_name=character_name)
+    from ZmxyOL.task.daily_task.bmkj.bmkj import task as bmkj_task
+    bmkj_task(clear_all=True, lianbao=False)
     ensure_in("登录")
 
 if __name__ == "__main__":
--- test/test_ensure_in.py
+++ test/test_ensure_in.py
@@ -4,7 +4,9 @@ import traceback
 
 if __name__ == "__main__":
     try:
-        ensure_in("极寒深渊", idx=1)
+        # ensure_in("极北")
+        # ensure_in("极寒深渊")
+        ensure_in("极北村庄")
 
     except Exception as e:
         traceback.print_exc()
